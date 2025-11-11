21cbc73d37bb5a7cb5fef04b5a4fe060c17e779b
tylerwight <tylermwight@gmail.com>
2025-11-11 12:11:54 -0700

    encounter structure

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,6 +6,9 @@ var stats: EnemyDb.EnemyData = EnemyDb.get_enemy("badboy")
 func setup_enemy(data: EnemyDb.EnemyData) -> void:
 	stats = data
 	print("I am enemy: ", data.name)
+	var sprite:= Sprite2D.new()
+	sprite.texture = load(stats.texture_path)
+	add_child(sprite)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
